737efa886d45582377a5fac2c9ca4b771f9b65fd
KVG24 <141866132+KVG24@users.noreply.github.com>
2024-12-31 22:19:47 +0000

    Implement Tree class and print function

--- src/bst.mjs
+++ src/bst.mjs
@@ -5,3 +5,34 @@ class Node {
         this.right = null;
     }
 }
+
+export class Tree {
+    constructor(array) {
+        const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+        this.root = this.buildTree(sortedArray);
+    }
+
+    buildTree(array) {
+        if (array.length === 0) return null;
+
+        let mid = Math.floor(array.length / 2);
+        let root = new Node(array[mid]);
+
+        root.left = this.buildTree(array.slice(0, mid));
+        root.right = this.buildTree(array.slice(mid + 1));
+
+        return root;
+    }
+}
+
+export function printTree(node, prefix = "", isLeft = true) {
+    if (node === null) return;
+
+    if (node.right !== null) {
+        printTree(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        printTree(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import "./bst.mjs";
+import { Tree, printTree } from "./bst.mjs";
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
+
+printTree(tree.root);
